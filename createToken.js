0beebd222b0fa5f66e8ada7f26f8a1a88aa001ed
Sven Anders Robbestad <141178592+blockchain-bros@users.noreply.github.com>
2024-03-24 11:42:28 +0100

    Parameterize token env

--- createToken.js
+++ createToken.js
@@ -17,17 +17,20 @@ import { SDrive } from "sdrive";
 const umi = createUmi(process.env.QUICKNODE_RPC);
 
 const userWallet = umi.eddsa.createKeypairFromSecretKey(
-  new Uint8Array(JSON.parse(process.env.WALLET)),
+  new Uint8Array(JSON.parse(process.env.PRIVATE_KEY)),
 );
 
 const userWalletSigner = createSignerFromKeypair(umi, userWallet);
 
 const sdrive = new SDrive(process.env.SDRIVE_API_KEY);
 sdrive.network = "ipfs";
-const imageURI = await sdrive.upload("images/token.png","token.png");
-const tokenName = "NAME";
-const tokenSymbol = "SYM";
-const tokenDescription = "DESCRIPTION";
+
+const image = process.env.TOKEN_IMAGE;
+
+const imageURI = await sdrive.upload(image,"token.png");
+const tokenName = process.env.TOKEN_NAME;
+const tokenSymbol = process.env.TOKEN_SYMBOL;
+const tokenDescription = process.env.TOKEN_DESCRIPTION;
 const tokenImage = imageURI.permalink; 
 const metadata = Buffer.from(
   JSON.stringify({
